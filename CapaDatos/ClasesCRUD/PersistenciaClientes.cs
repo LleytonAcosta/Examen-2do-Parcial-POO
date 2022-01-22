3c73d791b5ee7e98cddd9ce196fcd5c9b7fdd265
LleytonAcosta <Lleyton_200206@hotmail.com>
2022-01-21 19:33:20 -0500

    push

---
+++ CapaDatos/ClasesCRUD/PersistenciaClientes.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CapaDatos.Modelos;
+
+namespace CapaDatos.ClasesCRUD
+{
+    public static class PersistenciaClientes
+    {
+        public static List<Cliente> ListaClientes { get; set; }
+
+        //CRUD
+        //CREATE
+        public static void GuardarCliente(Cliente cliente)
+        {
+            ListaClientes.Add(cliente);
+        }
+
+        //UPDATE
+        public static void ModificarCliente(string cedula, string nombres, string apellidos, int edad)
+        {
+            Cliente cliente = ListaClientes.Find(x => x.Cedula == cedula);
+            cliente.Nombres = nombres;
+            cliente.Apellidos = apellidos;
+            cliente.Edad = edad;
+        }
+
+
+        //DELETE
+        public static void EliminarCliente(string cedula)
+        {
+            ListaClientes.RemoveAll(cliente => cliente.Cedula == cedula);
+        }
+
+        //READ
+        //UN OBJETO
+        public static Cliente BuscarCliente(string cedula)
+        {
+
+            return ListaClientes.Find(cliente => cliente.Cedula == cedula);
+        }
+
+        //LISTADO DE OBJETOS FORMATEADOS SEGÚN REQUERIMIENTO
+      public static string RetornarClientesEnString()
+        {
+
+            // Literal 1
+            //Logrado en la Clase Gestion cliente.cs de la capa de CaNegocios
+            /*List<string> client = (from data in ListaClientes
+                                   orderby data.Nombres ascending
+                                   select data.Nombres + " " + data.Apellidos).ToList();
+            return string.Join("\n", client);*/
+
+            //Literal 2
+            /*var Mayor18 = ListaClientes.OrderByDescending(mostrar => mostrar.Edad).Select(nomb => new { nomb.Nombres, nomb.Apellidos, nomb.Edad });
+            return string.Join("\n", Mayor18);*/
+
+
+            //litearl 4
+            /*var MayorEdad = ListaClientes.OrderByDescending(p => p.Edad).First();
+            return (string.Join(" ", MayorEdad.Nombres, " ", MayorEdad.Apellidos, " ","Con", MayorEdad.Edad, "Años ","\n"));*/
+
+            // literal 5
+            var Promed = ListaClientes.Average(j => j.Edad);
+            return (string.Join("\n", Promed));
+
+
+        }
+
+
+
+
+        // literal 3  Buscar Apellido de Cliente
+        /*public static string BuscarApellido(string apelli)
+        {
+            var add = ListaClientes.Where(i => i.Apellidos == apelli).Aggregate(" ", (center, datos) => center += $"-{datos.Apellidos}{datos.Nombres}\n").Select(i => i);
+            return (string.Join("", apelli));
+        }*/
+
+
+
+    }
+}
---
+++ CarritoDeCompras/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using CapaNegocios;
+using System.Collections.Generic;
+
+
+
+
+namespace CarritoDeCompras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GestionCliente cliente = new GestionCliente();                                                          
+            cliente.GuardarCliente("1112223334", "Michael Jackson", "Zambrano Zambrano", "michael@email.ec", "123",20);
+                                                                                                      
+            cliente.GuardarCliente("2223334445", "Juana Maria", "Pueblo Zambrano", "juan@mail.com", "123",19);
+
+            //Console.WriteLine(" 1) Los clientes ordenados alfabeticamente de A a Z. (1 Puntos)");
+
+            //Console.WriteLine("2) Los clientes mayores de 18 años ordenados alfabeticamente de Z a A. (2 Puntos)");
+
+            //Console.WriteLine("3) El cliente cuyo apellido sea un parametro ingresado por el usuario a traves del teclado. (2 Puntos)");
+
+            //Console.WriteLine(" Ingresar Apellido");
+            //string ingresar = Console.ReadLine();
+
+            //Console.WriteLine("Cliente encontrado :  \n"+ cliente.Ingresoapellido(ingresar));
+            //Console.WriteLine("4) El cliente de mayor edad. (2 Puntos)");
+            Console.WriteLine("5) El promedio de edad de los clientes. (1 Puntos)");
+            Console.WriteLine(cliente.ListarUsuarios());
+            Console.ReadKey();
+
+
+
+
+            // Muestra lo relacionado con Productos
+            /*GestionProductos producto = new GestionProductos();
+            Console.WriteLine(producto.ListarProductos());*/
+            
+
+            
+        }
+        
+           /*
+            4) El cliente de mayor edad. (2 Puntos)
+            5) El promedio de edad de los clientes. (1 Puntos)
+         */
+
+
+    }
+}
